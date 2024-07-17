3c5e1b0c124e9b87dc5e38676b47608dd4d5b3cc
Dtnkcreator <dtnk56@gmail.com>
2024-07-17 17:26:18 -0300

    1

--- main.py
+++ main.py
@@ -1,11 +1,37 @@
 import tkinter as tk
-class MainApp(tk.Tk):
+from controller.main_c import MainController
+
+class SampleApp(tk.Tk):
     def __init__(self):
-        super().__init__()
-        self.title("Jogo App")
-        self.geometry("400x400")
-        
+        tk.Tk.__init__(self)
+        self.title("Jogo")
+        self.geometry("300x300")
+        self._frame = None
+        self.switch_frame(Main)
+
+    def switch_frame(self, frame_class):
+        new_frame = frame_class(self)
+        if self._frame is not None:
+            self._frame.destroy()
+        self._frame = new_frame
+        self._frame.pack()
+
+
+class Main(tk.Frame):
+    def __init__(self, master):
+        tk.Frame.__init__(self, master)
+        tk.Label(self, text="Aplicativo de Jogos", font=("Helvetica", 18, "bold")).pack(
+            anchor="center",fill="x"
+        )
+        tk.Button(
+            self, text="Login", command=lambda: master.switch_frame(MainController.login)
+        ).pack()
+        tk.Button(
+            self, text="Permanecer desconectado", command=lambda: master.switch_frame(SemLogin)
+        ).pack()
+
 
 if __name__ == "__main__":
-    app = MainApp()
+    app = SampleApp()
     app.mainloop()
+
--- view/main_v.py
+++ view/main_v.py
@@ -9,11 +9,12 @@ class MainView(tk.Frame):
         self.create_widgets()
 
     def create_widgets(self):
-        self.button_login = ttk.Button(self, text="Login")
-        self.button_login.grid(row=0, column=0, padx=10, pady=5)
+        self.login_button = tk.Button(self, text="Login")
+        self.login_button.grid(row=0, column=0, padx=10, pady=5)
 
-        self.button_login = ttk.Button(self, text="Permanecer desconectado")
-        self.button_login.grid(row=1, column=0, padx=10, pady=5)
+        self.n_login_button = ttk.Label(self, text="Permanecer Desconectado")
+        self.n_login_button.grid(row=1, column=0, padx=10, pady=5)
+    
        
         self.grid_rowconfigure(3, weight=1)
         self.grid_columnconfigure(1, weight=1)
@@ -22,12 +23,4 @@ class MainView(tk.Frame):
     def mostrar_aviso_entrada(self):
         messagebox.showinfo("Aviso!", "Alguma das entradas não está completa! Verifique novamente!")
 
-    def get_nome(self):
-        return self.nome_entry.get()
 
-    def get_idade(self):
-        return self.idade_entry.get()
-    
-
-    def adicionar_usuario_lista(self, usuario):
-        self.usuarios_listbox.insert(tk.END, f"id {usuario[0]} | {usuario[1]} ({usuario[2]} anos)")
